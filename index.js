8a3b15350d218962a72ce206a2f834e5c9690b30
itscarew <itscarew@gmail.com>
2023-02-11 14:30:42 +0100

    new development on adding to cloud mongodb

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import redis from "redis";
+import cors from "cors";
 import pasteRoutes from "./routes/index.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const port = 5000;
 const redisPort = 6379;
 
 const app = express();
-const db = `mongodb://127.0.0.1:27017/pasteBinDB`;
-mongoose.connect(db);
+const db = `mongodb+srv://itscarew:${process.env.DB_PASSWORD}@paste-bin-cluster.kd40cmy.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
