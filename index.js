ae47cebf15452c55b081c08e3688ddbb5f081c0a
itscarew <itscarew@gmail.com>
2023-02-15 16:40:21 +0100

    redis implementation cloud

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const port = process.env.PORT || 5000;
-const redisPort =
-  `redis://default:${process.env.REDISPASSWORD}@containers-us-west-44.railway.app:7090` ||
-  6379;
+const redisPort = `redis://default:${process.env.REDISPASSWORD}@containers-us-west-44.railway.app:7090`;
 
 const app = express();
 const db = `mongodb+srv://itscarew:${process.env.DB_PASSWORD}@paste-bin-cluster.kd40cmy.mongodb.net/?retryWrites=true&w=majority`;
